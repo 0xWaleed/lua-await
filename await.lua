141a41e8454421fb7cb5169285c9c2f9f6fd8e82
0xWaleed <imwaleed@outlook.sa>
2021-05-19 23:53:36 +0300

    refactor: remove else clause and reformat the code 🎨♻️

--- await.lua
+++ await.lua
@@ -3,42 +3,46 @@
 --- DateTime: 5/18/21 11:02 PM
 ---
 
-await = setmetatable({}, {
-    __index = function(_, key)
-        local theFunction = _G[key]
-        if not theFunction then
-            return nil
-        end
-
-        return function(...)
-
-            local isYieldable   = coroutine.isyieldable()
-            local result        = {}
-            local isFinished    = false
-            local args          = { ... }
-            local argsCount     = #args
-
-            args[argsCount + 1] = function(...)
-                result     = { ... }
-                isFinished = true
+await =
+    setmetatable(
+    {},
+    {
+        __index = function(_, key)
+            local theFunction = _G[key]
+            if not theFunction then
+                return nil
             end
 
-            if isYieldable then
-                theFunction(table.unpack(args))
-                while not isFinished do
-                    coroutine.yield()
+            return function(...)
+                local isYieldable = coroutine.isyieldable()
+                local result = {}
+                local isFinished = false
+                local args = {...}
+                local argsCount = #args
+
+                args[argsCount + 1] = function(...)
+                    result = {...}
+                    isFinished = true
                 end
-                return table.unpack(result)
-            else
-                return coroutine.wrap(function(...)
+
+                if isYieldable then
                     theFunction(table.unpack(args))
                     while not isFinished do
                         coroutine.yield()
                     end
                     return table.unpack(result)
-                end)(...)
-            end
+                end
 
+                return coroutine.wrap(
+                    function(...)
+                        theFunction(table.unpack(args))
+                        while not isFinished do
+                            coroutine.yield()
+                        end
+                        return table.unpack(result)
+                    end
+                )(...)
+            end
         end
-    end
-})
+    }
+)
